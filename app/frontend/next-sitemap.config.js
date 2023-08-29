8a662e977a9dff87f931c8868f321699bd9816eb
Kai Pereira <kaipereira2020@gmail.com>
2023-08-29 10:11:55 -0700

    exclude dashboard pages

--- app/frontend/next-sitemap.config.js
+++ app/frontend/next-sitemap.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://re-auth.com'
+    siteUrl: "https://re-auth.com",
+    exclude: [
+        "/dashboard",
+        "/dashboard/*"
+    ]
 }
