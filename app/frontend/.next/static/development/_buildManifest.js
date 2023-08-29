473226fe0e8aa9fb67c4fbd38b71d76d1d428dac
Kai Pereira <kaipereira2020@gmail.com>
2023-08-29 10:05:04 -0700

    sitemap

--- app/frontend/.next/static/development/_buildManifest.js
+++ app/frontend/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[{source:"\u002F:path*\u002F_meta",destination:"\u002F404"}],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/docs":["static\u002Fchunks\u002Fpages\u002Fdocs.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdocs"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[{source:"\u002F:path*\u002F_meta",destination:"\u002F404"}],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
