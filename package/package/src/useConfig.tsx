87fed5b35d09582e03774a6aeda2c987ac6e0da9
Kai Pereira <kaipereira2020@gmail.com>
2023-08-30 14:47:03 -0700

    forgot to replace localhost with host

--- package/package/src/useConfig.tsx
+++ package/package/src/useConfig.tsx
@@ -6,8 +6,7 @@ const useAuth = () => {
     
     const { apiKey } = useContext(AuthContext)
 
-    // const baseUrl = process.env.BASE_URL || "https://re-auth-production.up.railway.app/"
-    const baseUrl = "http://localhost:4000/"
+    const baseUrl = process.env.BASE_URL || "https://re-auth-production.up.railway.app/"
 
     axios.defaults.baseURL = baseUrl; // Replace with your API base URL
     axios.defaults.headers.common["Authorization"] = apiKey;
